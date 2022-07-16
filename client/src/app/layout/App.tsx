d371a317a141570927ba9d4d7bcedd9df9cb7265
RichardKT88 <kendy_t88@hotmail.com>
2022-07-16 08:53:52 -0300

    [In Progress] Section 4: React Router

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { Container } from "@mui/system";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
 
 function App() {
@@ -24,7 +29,13 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/catalog' element={<Catalog />} />
+          <Route path='/catalog:id' element={<ProductDetails />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+        </Routes>
       </Container>
     </ThemeProvider>
   );
---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+export default function ProductDetails() {
+    return (
+        <Typography variant="h2">ProductDetails</Typography>
+    )
+}
