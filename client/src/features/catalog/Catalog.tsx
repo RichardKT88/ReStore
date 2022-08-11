5350246fea44e1a4524bdcf67f54e240fc8fa7d8
RichardKT88 <kendy_t88@hotmail.com>
2022-08-11 17:27:30 -0300

    [Progress] Section 8: Paging, Sorting, Filtering

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,12 +1,20 @@
+import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, Grid, Pagination, Paper, Radio, RadioGroup, Typography } from "@mui/material";
 import { useEffect } from "react";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
 import ProductList from "./ProductList";
+import ProductSearch from "./ProductSearch";
+
+const sortOptions = [
+    { value: 'name', label: 'Alphabetical' },
+    { value: 'priceDesc', label: 'Price - High to low' },
+    { value: 'price', label: 'Price - Low to high' },
+]
 
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const { productsLoaded, status } = useAppSelector(state => state.catalog);
+    const { productsLoaded, status, filtersLoaded, brands, types } = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -20,11 +28,59 @@ export default function Catalog() {
             .then(data => setProducts(data))*/
     }, [productsLoaded, dispatch])
 
+    useEffect(() => {
+        if (!filtersLoaded) dispatch(fetchFilters());
+    }, [dispatch, filtersLoaded])
+
     if (status.includes('pending')) return <LoadingComponent message='Loading products...' />
 
     return (
-        <>
-            <ProductList products={products} />
-        </>
+        <Grid container spacing={4}>
+            <Grid item xs={3}>
+                <Paper sx={{ mb: 2 }}>
+                    <ProductSearch />
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <FormControl component="fieldset">
+                        <RadioGroup>
+                            {sortOptions.map(({ value, label }) => (
+                                <FormControlLabel value={value} control={<Radio />} label={label} key={value} />
+                            ))}
+                        </RadioGroup>
+                    </FormControl>
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <FormGroup>
+                        {brands.map(brand => (
+                            <FormControlLabel control={<Checkbox />} label={brand} key={brand} />
+                        ))}
+                    </FormGroup>
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <FormGroup>
+                        {types.map(type => (
+                            <FormControlLabel control={<Checkbox />} label={type} key={type} />
+                        ))}
+                    </FormGroup>
+                </Paper>
+            </Grid>
+            <Grid item xs={9}>
+                <ProductList products={products} />
+            </Grid>
+            <Grid item xs={3} />
+            <Grid item xs={9}>
+                <Box display='flex' justifyContent='space-between' alignItems='center'>
+                    <Typography>
+                        Displaying 1-6 of 20
+                    </Typography>
+                    <Pagination
+                        color="secondary"
+                        size='large'
+                        count={10}
+                        page={2}
+                    />
+                </Box>
+            </Grid>
+        </Grid>
     )
 }
--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -1,15 +1,36 @@
 import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 import agent from "../../app/api/agent";
-import { Product } from "../../app/models/product";
+import { Product, ProductParams } from "../../app/models/product";
 import { RootState } from "../../app/store/configureStore";
+
+interface CatalogState {
+    productsLoaded: boolean;
+    filtersLoaded: boolean;
+    status: string;
+    brands: string[];
+    types: string[];
+    productParams: ProductParams;
+}
 
 const productsAdapter = createEntityAdapter<Product>();
 
-export const fetchProductsAsync = createAsyncThunk<Product[]>(
+function getAxiosParams(productParams: ProductParams){
+    const params = new URLSearchParams();
+    params.append('pageNumber', productParams.pageNumber.toString());
+    params.append('pageSize', productParams.pageSize.toString());
+    params.append('orderBy', productParams.orderBy);
+    if (productParams.searchTerm) params.append('searchTerm', productParams.searchTerm);
+    if (productParams.brands) params.append('brands', productParams.brands.toString());    
+    if (productParams.types) params.append('types', productParams.types.toString());
+    return params;    
+}
+
+export const fetchProductsAsync = createAsyncThunk<Product[], void, {state: RootState}>(
     'catalog/fetchProductsAsync',
     async (_, thunkAPI) => {
+        const params = getAxiosParams(thunkAPI.getState().catalog.productParams);
         try {
-            return await agent.Catalog.list();
+            return await agent.Catalog.list(params);
         } catch (error: any) {
             return thunkAPI.rejectWithValue({error: error.data})
         }
@@ -26,14 +47,44 @@ export const fetchProductAsync = createAsyncThunk<Product, number>(
         }
     }
 )
+export const fetchFilters = createAsyncThunk(
+    'catalog/fetchFilters',
+    async (_, thunkAPI) => {
+        try {
+            return agent.Catalog.fetchFilters();
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({error: error.data})
+        }
+    }
+)
+
+function initParams() {
+    return {
+        pageNumber: 1,
+        pageSize: 6,
+        orderBy: 'name'
+    }
+}
 
 export const catalogSlice = createSlice({
     name: 'catalog',
-    initialState: productsAdapter.getInitialState({
+    initialState: productsAdapter.getInitialState<CatalogState>({
         productsLoaded: false,
-        status: 'idle'
+        filtersLoaded: false,
+        status: 'idle',
+        brands: [],
+        types: [],
+        productParams: initParams()
     }),
-    reducers: {},
+    reducers: {
+        setProductParams:(state, action) => {
+            state.productsLoaded = false;
+            state.productParams = {...state.productParams, ...action.payload};
+        },
+        resetProductParams:(state) => {
+            state.productParams = initParams();
+        }
+    },
     extraReducers: (builder => {
         builder.addCase(fetchProductsAsync.pending, (state) => {
             state.status = 'pendingFetchProducts';
@@ -57,8 +108,23 @@ export const catalogSlice = createSlice({
         builder.addCase(fetchProductAsync.rejected, (state, action) => {
             console.log(action);
             state.status = 'idle';
-        })
+        });
+        builder.addCase(fetchFilters.pending, (state) => {
+            state.status = 'pendingFetchFilters';
+        });
+        builder.addCase(fetchFilters.fulfilled, (state, action) => {
+            state.brands = action.payload.brands;
+            state.types = action.payload.types;
+            state.filtersLoaded = true;
+            state.status = 'idle';
+        });
+        builder.addCase(fetchFilters.rejected, (state, action) => {
+            state.status = 'idle';
+            console.log(action.payload);
+        });
     })
 })
 
 export const productSelectors = productsAdapter.getSelectors((state: RootState) => state.catalog);
+
+export const {setProductParams, resetProductParams} = catalogSlice.actions;
